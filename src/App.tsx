fd069d823abc9088cb20563c16c2d0714ead8281
g9gbka-create <g9gbka@gmail.com>
2026-07-09 15:47:56 +0300

    update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
+
 import { Header } from './components/Header';
 import { BottomNav } from './components/BottomNav';
+
 import { HomePage } from './pages/HomePage';
 import { CatalogPage } from './pages/CatalogPage';
 import { ProductPage } from './pages/ProductPage';
@@ -18,28 +20,21 @@ function App() {
       <AuthProvider>
         <CartProvider>
           <FavoritesProvider>
-            <div className="min-h-screen bg-white">
-              <Routes>
-                <Route path="/admin" element={<AdminPage />} />
-                <Route
-                  path="*"
-                  element={
-                    <>
-                      <Header />
-                      <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/catalog" element={<CatalogPage />} />
-                        <Route path="/product/:slug" element={<ProductPage />} />
-                        <Route path="/cart" element={<CartPage />} />
-                        <Route path="/favorites" element={<FavoritesPage />} />
-                        <Route path="/profile" element={<ProfilePage />} />
-                      </Routes>
-                      <BottomNav />
-                    </>
-                  }
-                />
-              </Routes>
-            </div>
+
+            <Header />
+
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/catalog" element={<CatalogPage />} />
+              <Route path="/product/:slug" element={<ProductPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/favorites" element={<FavoritesPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/admin" element={<AdminPage />} />
+            </Routes>
+
+            <BottomNav />
+
           </FavoritesProvider>
         </CartProvider>
       </AuthProvider>
