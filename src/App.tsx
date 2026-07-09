212cc35c77f265ec1cdb52707b38d65b4e6577ba
g9gbka-create <g9gbka@gmail.com>
2026-07-09 15:53:12 +0300

    update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { CartProvider } from './contexts/CartContext';
-import { FavoritesProvider } from './contexts/FavoritesContext';
-
-import { Header } from './components/Header';
-import { BottomNav } from './components/BottomNav';
-
-import { HomePage } from './pages/HomePage';
-import { CatalogPage } from './pages/CatalogPage';
-import { ProductPage } from './pages/ProductPage';
-import { CartPage } from './pages/CartPage';
-import { FavoritesPage } from './pages/FavoritesPage';
-import { ProfilePage } from './pages/ProfilePage';
-import { AdminPage } from './pages/AdminPage';
-
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <FavoritesProvider>
-
-            <Header />
-
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/catalog" element={<CatalogPage />} />
-              <Route path="/product/:slug" element={<ProductPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/favorites" element={<FavoritesPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/admin" element={<AdminPage />} />
-            </Routes>
-
-            <BottomNav />
-
-          </FavoritesProvider>
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <h1 style={{ padding: "40px", fontSize: "40px" }}>
+      EasyShop работает!
+    </h1>
   );
 }
 
